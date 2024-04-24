858a9b781599ae5246834dc85e25ce863df48290
Jirameth Kaewsuwan <meth.jirameth@gmail.com>
2024-04-24 18:02:40 +0700

    Update - 5

--- dev-next/pages/product/index.js
+++ dev-next/pages/product/index.js
@@ -1,6 +1,7 @@
 import styles from '@/styles/product.module.css'
 import Image from 'next/image';
 import Head from 'next/head';
+import Link from 'next/link';
 
 
 export async function getStaticProps(){
@@ -27,15 +28,20 @@ export default function Product(props){
                 <Image src='/store.jpg' alt='about' width={200} height={100}/>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni neque incidunt perspiciatis harum illum ipsum autem eius odio mollitia laudantium.</p>
                 
+                <div className={styles['container-item']}>
                 {
                     productData.map(dataArray=>{
                         return  (
-                            <div key={dataArray.id}>
-                                {dataArray.title} <Image src={dataArray.thumbnail} alt={dataArray.title} width={50} height={50}/>
+                            <div key={dataArray.id} className={styles['item-box']}>
+                                    <div className={styles['title-item-box']}>{dataArray.title}</div>
+                                    <Link href={'/product/' + dataArray.id}>
+                                        <Image src={dataArray.thumbnail} alt={dataArray.title} width={300} height={300}/>
+                                    </Link>
                             </div>
                         );
                     })
                 }
+                </div>
 
             </div>
         </>
